055880c87aff822a51e23fd72643618416fc6965
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-14 16:47:49 -0600

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,7 +4,7 @@
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure the height
  *
- * Return: Height of the tree, 0 if tree is NULL
+ * Return: Height of the tree, 0 if tree is NULL or if it's a leaf node
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -13,9 +13,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
     if (tree == NULL)
         return 0;
 
+    if (tree->left == NULL && tree->right == NULL)
+        return 0;
+
     left_height = binary_tree_height(tree->left);
     right_height = binary_tree_height(tree->right);
 
     return (1 + (left_height > right_height ? left_height : right_height));
 }
-
