01a4b095f924a8a46b20a67672b17f6dadf161b1
Manning Stinson <manning@manningstinson.com>
2023-12-15 07:02:33 -0800

    betty styles

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,12 +6,13 @@
  *
  * Return: Number of nodes with at least 1 child, 0 if tree is NULL
  */
+
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 	if (tree->left || tree->right)
-		return (91 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 	else
 		return (0);
 }
