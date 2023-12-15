4b48b8878c93d5ce9de04618989dad90519e2710
Manning Stinson <manning@manningstinson.com>
2023-12-15 06:55:48 -0800

    betty styles

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,11 +8,10 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
-
-    if (tree->left || tree->right)
-        return 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
-    else
-        return 0;
+	if (tree == NULL)
+		return (0);
+	if (tree->left || tree->right)
+		return (91 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	else
+		return (0);
 }
